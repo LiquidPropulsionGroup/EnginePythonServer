5a5aef41c99081fb44b49f260c218cbf5ef1dc5f
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-04-17 02:19:51 -0700

    controller status code

---
+++ controller status/index.py
@@ -0,0 +1,42 @@
+from flask import Flask, abort
+import serial, sys
+
+####* User defined variables START *####
+try:
+    sys.argv[1]
+except IndexError:
+    baudrate = 9600 # defult value
+else:
+    baudrate = sys.argv[1]
+
+try:
+    sys.argv[2]
+except IndexError:
+    port = '/dev/controller_main' # defult value
+else:
+    port = sys.argv[2]
+####! User defined variables END !####
+
+# Flask app settings
+app = Flask(__name__)
+
+# Serial port settings
+ser = serial.Serial()
+ser.baudrate = baudrate
+ser.port = port
+
+# Openning serial port
+ser.open()
+
+@app.route('/serial/controller/<action>')
+def main_valve_route(action):
+  if ser.is_open == True:
+    if action == 'STATUS':
+      ser.write(b'STATUS\n')
+      message = ser.readline()
+      return message.decode('UTF-8')
+
+    return abort(404)
+
+if __name__ == '__main__':
+      app.run(host='192.168.0.11', port=3003)
