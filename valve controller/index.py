236e4716bd2e9e7f6437ca869e7f800e4f93720e
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2021-10-01 18:30:48 -0700

    Testing on Pi

--- valve controller/index.py
+++ valve controller/index.py
@@ -16,17 +16,17 @@ try:
     sys.argv[2]
 except IndexError:
     # For use in desktop environment:
-    ports = serial.tools.list_ports.comports()
-    print(ports)
-    com_list = []
-    for p in ports:
-          com_list.append(p.device)
-    print(com_list)
-    port = com_list[1]
-    print(port)
+    # ports = serial.tools.list_ports.comports()
+    # print(ports)
+    # com_list = []
+    # for p in ports:
+    #       com_list.append(p.device)
+    # print(com_list)
+    # port = com_list[1]
+    # print(port)
 
     # For use in live environment
-    # port = '/dev/controller_valve' # defult value
+    port = '/dev/controller_valve' # defult value
 else:
     port = sys.argv[2]
 
@@ -150,9 +150,9 @@ def valve_update():
       print(json_data)
 
       # Insert to redis
-      # if json_data:
-      #   redis.xadd(stream_name, json_data)
-      #   print('Added to redis stream')   
+      if json_data:
+        redis.xadd(stream_name, json_data)
+        print('Added to redis stream')   
 
     return "Sent + Received"
 
