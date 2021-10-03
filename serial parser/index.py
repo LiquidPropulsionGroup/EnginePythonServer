e5312485fa44afd93ff6385acf5cfa4aab678861
TWCCarlson <thomaswccarlson@gmail.com>
2021-10-03 08:03:45 -0700

    Troubleshoot state, valve reporting works with delay

--- serial parser/index.py
+++ serial parser/index.py
@@ -52,6 +52,7 @@ CORS(app) # Enables POST requests
 
 # Lock a thread
 lock = threading.Lock()
+serial_lock = threading.Lock()
 
 # Creating redis client instance
 redis = red.Redis(host='redis-database', port=6379)
@@ -88,7 +89,6 @@ Sensor_Keys = [
 
 # JSON Key list for valve status
 Valve_Keys = [
-    "Packet_Start",
     "Timestamp",
     "FUEL_Press",
     "LOX_Press",
@@ -97,7 +97,6 @@ Valve_Keys = [
     "MAIN",
     "FUEL_Purge",
     "LOX_Purge",
-    "Packet_End"
 ]
 
 def run_app():
@@ -113,19 +112,19 @@ def Cache(ser, redis, caching_stream_name, valve_stream_name):
     VALVE_BUFFER_LENGTH = 19
 
     # Flush the input buffer to avoid overflow and get fresh data
-    ser.reset_input_buffer
+    ser.reset_input_buffer()
 
     # Both are padded with a starter sequence of 4 zero bytes
     # and terminated with a sequence of 4 max bytes
     # Start the loop in the right place by finding a terminator string in the buffer
-    serial_buffer = ser.read_until(b'\xFF\xFF\xFF\xFF\x00\x00\x00\x00')
+    # serial_buffer = ser.read_until(b'\xFF\xFF\xFF\xFF\x00\x00\x00\x00')
 
-    while ser.is_open == True:
+    while True:
         if CACHING:
-            print("LOOPING")
+            #print("LOOPING")
             # Extract the next sequence of serial data until the terminator/starter packets
             serial_buffer = ser.read_until(b'\xFF\xFF\xFF\xFF\x00\x00\x00\x00')
-
+            #print(serial_buffer)
             # If the serial buffer has a length of 40 bytes, it is a sensor data package
             if len(serial_buffer) == SENSOR_BUFFER_LENGTH:
                 # Unpack the struct that is the serial message
@@ -135,7 +134,7 @@ def Cache(ser, redis, caching_stream_name, valve_stream_name):
                 data = {}
                 for item in range(len(Sensor_Keys)):
                     data[Sensor_Keys[item]] = str(unpack_data[item])
-                print(data)
+                #print(data)
                 json_data = json.dumps(data)
                 json_data = json.loads(json_data)		# Weird fix?
 
@@ -149,7 +148,7 @@ def Cache(ser, redis, caching_stream_name, valve_stream_name):
                 # Arduino is little-endian
                 # Unpack the struct that is the serial message
                 # Arduino is little-endian
-                unpack_data = struct.unpack('<I i b b b b b b b I', serial_buffer)
+                unpack_data = struct.unpack('<i b b b b b b b d', serial_buffer)
                 print(unpack_data)
                 # Build the JSON with struct method
                 data = {}
@@ -163,26 +162,29 @@ def Cache(ser, redis, caching_stream_name, valve_stream_name):
                 # Insert to redis
                 if json_data:
                     redis.xadd(valve_stream_name, json_data)
-                    # print("Added to redis stream")
+                    print("Added to redis stream")
 
             else:
                 # If the buffer length is improper, the message is invalid and should be discarded
                 print("=====INVALID MESSAGE=====")
+                print(serial_buffer)
+        # else:
+            # print("Not caching...")
 
 def compose_pair(key, state, instruction):
-    if key == Valve_Keys[2]:
+    if key == Valve_Keys[1]:
         leadByte = b'\x53'    # FUEL_Pres(S)
+    elif key == Valve_Keys[2]:
+        leadByte = b'\x73'    # FUEL_Pres(s)
     elif key == Valve_Keys[3]:
-        leadByte = b'\x73'    # FUEL_Pres(S)
-    elif key == Valve_Keys[4]:
         leadByte = b'\x54'    # FUEL_Ven(T)
-    elif key == Valve_Keys[5]:
+    elif key == Valve_Keys[4]:
         leadByte = b'\x74'    # LOX_Ven(t)
-    elif key == Valve_Keys[6]:
+    elif key == Valve_Keys[5]:
         leadByte = b'\x4D'    # (M)ain
-    elif key == Valve_Keys[7]:
+    elif key == Valve_Keys[6]:
         leadByte = b'\x45'    # FUEL_Purg(E)
-    elif key == Valve_Keys[8]:
+    elif key == Valve_Keys[7]:
         leadByte = b'\x65'    # FUEL_Purg(e)
 
     if state == True:
@@ -196,7 +198,7 @@ def compose_pair(key, state, instruction):
 if __name__ == "__main__":
     # Threading the routes
     flaskApp_thread = threading.Thread(target=run_app)
-    caching_thread = threading.Thread(target=Cache, args=[ser, caching_stream_name, valve_stream_name])
+    caching_thread = threading.Thread(target=Cache, args=[ser, redis, caching_stream_name, valve_stream_name])
     flaskApp_thread.start()
     caching_thread.start()
 
@@ -239,7 +241,7 @@ def serialSend():
         # Instruction starter character '<'
         instruction = b'\x3C'
         # Pull the items out of the JSON object using the key list
-        for key in Valve_Keys[2:9]:
+        for key in Valve_Keys[1:]:
             print(key)
             print(int(message[key]))
             # Pair up the keys and the instruction values
@@ -253,9 +255,10 @@ def serialSend():
         # Generate a polling message for the Arduino
         # A string of same length as the instruction message for simplicity
         status_request_char = b'\x3F'
-        status_request = ''
-        for i in range(1,14):
+        status_request = b'\x3C'
+        for i in range(0,14):
             status_request += status_request_char
+        status_request += b'\x3E'
         ser.write(status_request)
         print(status_request)
 
