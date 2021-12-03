d58eb454081025b6f094da925a1d764256e49c22
TWCCarlson <thomaswccarlson@gmail.com>
2021-12-02 20:55:41 -0800

    storage working

--- storage database/index.py
+++ storage database/index.py
@@ -18,15 +18,16 @@ app = Flask(__name__)
 # Creating redis client
 redis = red.Redis(host='redis-database', port=6379)
 
-# Flow control variable
-operation = False
-
 def Store():
+    global operation
+
     # Connect to the sqlite3 database
     connection = sqlite3.connect('database.db')
+    print("db connected")
 
     # Establish the cursor to do operations on the .db
     cursor = connection.cursor()
+    print("cursor created")
 
     # Creating the table to store data, if it doesn't exist already
     # create_table stores the SQL command to be executed by cursor
@@ -50,27 +51,36 @@ def Store():
     cursor.execute(create_table)
     # Commit changes to the .db
     connection.commit()
+    print("table created")
 
     # Get the first item in the stream to use XREAD
     data = redis.xrange(stream_name, count=1)
     (label, data) = data[0]
+    print(label)
+    print(data)
 
     # Use XREAD to get the first set of data
     data = redis.xread({ stream_name: f'{label.decode()}' }, block = 0)
     (label, data) = data[0]
+    #print(label)
+    #print(data)
 
     # Entering the storage loop for as long as operation is true
     while operation == True:
+        print("looping...")
         for sensor_reading in data:
             # Separate the tuples
             (sensor_label, sensor_data) = sensor_reading
+            #print(sensor_label)
+            #print(sensor_data)
 
             # Split the redis timestamp using regex
             sensor_timestamp = re.split("-", sensor_label.decode())
+            print(sensor_timestamp)
 
             # Write the SQL command to add the data to the .db
             insert_string = f""" INSERT INTO {stream_name} VALUES 
-            ( {sensor_timestamp},
+            ( {sensor_timestamp[0]},
             {sensor_data[b'PT_HE'].decode()},
             {sensor_data[b'PT_Purge'].decode()},
             {sensor_data[b'PT_Pneu'].decode()},
@@ -85,26 +95,28 @@ def Store():
             {sensor_data[b'TC_CHAM'].decode()},
             {sensor_data[b'FT_Thrust'].decode()},
             {sensor_data[b'FL_WATER'].decode()} );"""
+            print("insert command generated")
 
             # Execute the SQL command
             cursor.execute(insert_string)
+            print("insert command executed")
 
             # Commit changes
             connection.commit()
-        
+            print("insert committed")
+
         # Find the next set of redis data
         data = redis.xrange(stream_name, min=f'{sensor_label.decode()}', count=1)
         (label, data) = data[0]
         data = redis.xread({ stream_name: f'{label.decode()}' }, block = 0)
         (label, data) = data[0]
-            
+        print("new data found")
 
 
 @app.route('/serial/storage/<action>')
 def storage_control(action):
     if action == 'START':
         print('STORAGE START')
-        # Set the global control variable to allow execution of storage
         global operation
         operation = True
 
@@ -115,14 +127,16 @@ def storage_control(action):
         return 'Storage crashed'
 
     if action == 'CLOSE':
-        # Set the global control variable to stop storage execution
-        global operation
         operation = False
-        
+
         # Indicate storage stopped
         return 'Storage closed'
 
     return abort(404)
+
+# Flow control variable
+global operation
+operation = False
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3004, threaded=True)
